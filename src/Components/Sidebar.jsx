a7c1a41bfaf2cd58af07b7b6c0999d30d98bcab6
Abubokkor98 <mail.abubokkor@gmail.com>
2024-10-22 18:54:59 +0600

    display want to cook section

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
 export default function Sidebar({recipeQueue}) {
-  console.log(recipeQueue);
+
   return (
-    <div className='md:w-1/3'>
-      Want to cook: {recipeQueue.length}
+    <div className='md:w-1/3 border-2 rounded-2xl text-gray-600 p-2 bg-base-100'>
+      {/* want to cook table */}
+      <div className="overflow-x-auto">
+        <h2 className='border-b-2 mx-auto font-semibold text-2xl text-gray-800 text-center pb-2'>Want To Cook: {recipeQueue.length}</h2>
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Time</th>
+        <th>Calories</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        recipeQueue.map((recipe,index)=>
+        <tr key={index} className="hover">
+        <th>{index + 1}</th>
+        <td>{recipe.recipe_name}</td>
+        <td>{recipe.preparing_time}</td>
+        <td>{recipe.calories}</td>
+        <td><button onClick={()=>console.log('Preparing')} className="bg-green-400 rounded-full text-sm text-gray-800 font-medium px-4 py-2">Preparing</button></td>
+      </tr>)
+      }
+
+      
+    </tbody>
+  </table>
+</div>
+      {/* currently cooking table */}
     </div>
   )
 }
